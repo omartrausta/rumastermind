0be2f5b45273ea93a66ebda9936b0f6e3dc76aa9
gunnlaugursig@gmail.com <gunnlaugursig@gmail.com@1b2a0b22-966c-11de-890a-fda65104c1fd>
2009-08-31 22:52:36 +0000

    Setti inn unittest fyrir colorsToPlay

--- src/test_mastermind.py
+++ src/test_mastermind.py
@@ -1,4 +1,13 @@
+# encoding: utf-8
+import unittest, mastermind, os
 
-    
-
-
+class mastermindTestCase(unittest.TestCase):    
+    colors = ["Gulur","Rauður","Grænn","Blár","Svartur","Hvítur","Fjólublár"]
+    number=3
+     
+    def testColorsToPlay(self):
+        result = mastermind.colorsToPlay(self.number)
+        print result
+        #self.assertEqual(result, self.expected, 'Test féll')
+        
+if __name__ == '__main__': unittest.main()
