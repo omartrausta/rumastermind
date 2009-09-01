33eb32ac0cc0047f449b421ceeb98d4576d91e98
gunnlaugursig@gmail.com <gunnlaugursig@gmail.com@1b2a0b22-966c-11de-890a-fda65104c1fd>
2009-09-01 23:17:19 +0000

    Breytti yfir í class

--- src/mastermind.py
+++ src/mastermind.py
@@ -1,21 +1,21 @@
 # encoding: utf-8
 import random
 
+class Mastermind:
 
-colors = ["Gulur","Rauður","Grænn","Blár","Svartur","Hvítur","Fjólublár","Brúnn"]
+    colors = ["Gulur","Rauður","Grænn","Blár","Svartur","Hvítur","Fjólublár","Brúnn"]
 
-def shuffleColors():
-    random.shuffle(colors)
+    def shuffleColors():
+        random.shuffle(colors)
     
-def gradePlayerChoice():
-    return
+    def gradePlayerChoice():
+        return
 
-def colorsToPlay(numberOfColors):
-    return colors[numberOfColors:]
+    def colorsToPlay(numberOfColors):
+        return colors[numberOfColors:]
 
-def main():
-    shuffleColors()
-    print colorsToPlay(3)
+    def main():
+        shuffleColors()
+        print colorsToPlay(3)
 
-
-if __name__== "__main__": main()
+    if __name__== "__main__": main()
