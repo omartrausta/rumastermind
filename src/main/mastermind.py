da0d67235be769db57c73da781b0768575aaf5f7
davidhalldor <davidhalldor@1b2a0b22-966c-11de-890a-fda65104c1fd>
2009-09-02 15:22:26 +0000

    Mastermind gagnavinnslan nánast tilbúin, bara logík og client forritun eftir

--- src/main/mastermind.py
+++ src/main/mastermind.py
@@ -2,9 +2,12 @@
 import random
 
 class Mastermind:  
-    def __init__(self,colors):
+    def __init__(self,colors,guessColors,sameColor=False):
         self.colors = colors
         self.shuffleColors()
+        self.rows = range(guessColors)
+        self.sameColor = sameColor
+        self.totalGuesses = 0
         
     def shuffleColors(self):
         random.shuffle(self.colors)
@@ -14,6 +17,15 @@ class Mastermind:
 
     def colorsToPlay(self,numberOfColors):
         return self.colors[0:numberOfColors]
+    
+    def addToRows(self,row):
+        self.rows.remove(self.totalGuesses)
+        self.rows.insert(self.totalGuesses,row)
+        self.totalGuesses =+ 1
+    
+    def populateRows(self):
+        for row in self.rows:
+            print row
     
 def main():
     colors = ["Gulur","Rauður","Grænn","Blár","Svartur","Hvítur","Fjólublár","Brúnn"]
--- src/test/test_mastermind.py
+++ src/test/test_mastermind.py
@@ -1,21 +1,39 @@
 # encoding: utf-8
 import unittest 
 from main.mastermind import Mastermind
+from main.color import Color
+from main.row import Row
 
 class MastermindTestCase(unittest.TestCase):    
-    colors = ["Gulur","Rauður","Grænn","Blár","Svartur","Hvítur","Fjólublár"]
-    number=3
-    guess = ["gulur", "svartur"]
-    expected = ['Gulur', 'Bl\xc3\xa1r', 'Svartur']
+    gulur = Color("gulur", "path")
+    raudur = Color("rauður", "path")
+    graenn = Color("graenn", "path")
+    blar = Color("blaur","path")
+    colors = [gulur,raudur,graenn,blar]
+    numberOfColors=2
+    numberOfGuesses=8
+    sameColor=True
+    guess1 = [gulur,graenn]
+    guess2 = [graenn,blar]
+    expected = [blar,graenn]
      
     def testColorsToPlay(self):
-        result = Mastermind(self.colors)
-        result = result.colorsToPlay(self.number)
+        result = Mastermind(self.colors,self.numberOfGuesses)
+        result = result.colorsToPlay(self.numberOfColors)
         self.assertEqual(result, self.expected)
 
     def testGradePlayerChoice(self):
-        result = Mastermind(self.colors)
+        result = Mastermind(self.colors,self.numberOfGuesses)
         result = result.gradePlayerChoice()
         self.assertEqual(result,None)
+        
+    def testRows(self):
+        result = Mastermind(self.colors,self.numberOfGuesses)
+        result.colorsToPlay(self.numberOfColors)
+        row1 = Row(self.guess1)
+        result.addToRows(row1)
+        row2 = Row(self.guess2)
+        result.addToRows(row2)
+        result.populateRows()
 
 if __name__ == '__main__': unittest.main()
