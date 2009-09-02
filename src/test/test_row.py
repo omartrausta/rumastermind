1b10a0935932a41f7ca85d3a7cfa9008a3784561
davidhalldor <davidhalldor@1b2a0b22-966c-11de-890a-fda65104c1fd>
2009-09-02 14:33:05 +0000

    Smá test á Row

--- src/test/test_row.py
+++ src/test/test_row.py
@@ -1,5 +1,6 @@
 import unittest
 from main.row import Row
+from main.color import Color
  
 class rowTest(unittest.TestCase):
     
@@ -7,8 +8,15 @@ class rowTest(unittest.TestCase):
     
     def testRow(self):
         result = Row(self.colorList)
+        gulur = Color("gulur","path")
+        result.colorList.append(gulur)
         print result.hintBlack
         print result.hintWhite 
-        self.assertEqual(len(result.colorList),0)
+        result.hintBlack =+ 1
+        result.hintWhite =+ 1
+        print result.hintBlack
+        print result.hintWhite
+        print result.colorList
+        self.assertEqual(len(result.colorList),1)
         
 if __name__ == '__main__': unittest.main()
