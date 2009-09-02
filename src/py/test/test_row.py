b8070dff183aff69328976db6a4853917f7dfbdf
davidhalldor <davidhalldor@1b2a0b22-966c-11de-890a-fda65104c1fd>
2009-09-02 00:10:21 +0000

    Test

--- src/py/test/test_row.py
+++ src/py/test/test_row.py
@@ -1,1 +1,12 @@
 import unittest
+import py.main.row
+
+class rowTest(unittest.TestCase):
+    
+    colorList = []
+    
+    def testRow(self):
+        result = row.Row(self.colorList)
+        self.assertEqual(len(result.colorList, 0))
+
+if __name__ == '__main__': unittest.main()
